87bea557cfda728f4702acfbce80615eef8639c1
Mako <mnfeconicu41@gmail.com>
2016-07-08 15:21:22 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+Object.prototype.values = function () {
+  var o = this;
+  var r = [];
+  for (var k in o)
+    if (o.hasOwnProperty(k))
+      r.push(o[k]);
+  return r;
+};
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+var waitingPlayers = [];
+
+io.on('connection', function (socket) {
+  waitingPlayers.push(socket);
+
+  // default room is "waitingPlayers"
+  socket.room = 'waitingPlayers';
+  socket.join(socket.room);
+
+  io.to(socket.id).emit('chat message', `Current Room: ${socket.room}.`);
+
+  socket.on('disconnect', function () {
+    io.to(socket.room).emit('chat message', `user (${socket.id}) disconnected`);
+    if (socket.room == 'waitingPlayers') {
+      // delete this socket from waitingPlayers
+      var index = waitingPlayers.indexOf(socket);
+      waitingPlayers.splice(index, 1);
+    }
+  });
+
+  socket.on('chat message', function (msg) {
+    io.to(socket.room).emit('chat message', msg);
+  });
+
+  // whether 2 players are online.
+  if (waitingPlayers.length >= 2) {
+    var roomName = socket.id;
+    waitingPlayers.splice(0, 2).map(function (socket) {
+      socket.leave(socket.room); // leave from default room
+      socket.room = roomName;
+      socket.join(roomName); // join to new room
+    });
+    io.to(roomName).emit('chat message', `Current Room: ${socket.room}.`);
+  }
+});
+
+http.listen(3000, function () {
+  console.log('listening on :3000');
+});
